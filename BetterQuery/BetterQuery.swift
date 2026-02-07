58aedf76e6c02edcf57e1bbd5bc954a1efeab0ba
Nils Fischer <nils-fischer7@web.de>
2026-02-07 12:36:06 +0100

    Initial Commit

---
+++ BetterQuery/BetterQuery.swift
@@ -0,0 +1,9 @@
+//
+//  BetterQuery.swift
+//  BetterQuery
+//
+//  Created by Nils Fischer on 07.02.26.
+//
+
+import Foundation
+
---
+++ BetterQueryTests/BetterQueryTests.swift
@@ -0,0 +1,17 @@
+//
+//  BetterQueryTests.swift
+//  BetterQueryTests
+//
+//  Created by Nils Fischer on 07.02.26.
+//
+
+import Testing
+@testable import BetterQuery
+
+struct BetterQueryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
